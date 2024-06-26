a870bbbc7275d6a56c3d38b02f8accbc0adc2302
adilavirk <adilavirk0@gmail.com>
2024-06-26 13:26:34 +0500

    made some changes in code

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
 // to connect with mongo db serve
 const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/inotebook";
+const mongoURI =
+  "mongodb+srv://adilavirk:adilavirk6083109@cluster0.xi41hsp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 mongoose.set("strictQuery", false);
 // to connect with db
 const connectToMongo = () => {
-  mongoose.connect(mongoURI, () => {
-    console.log("connected to mongo successfuly");
-  });
+  try {
+    mongoose.connect(mongoURI, () => {
+      console.log("connected to mongo successfuly");
+    });
+  } catch (error) {
+    console.log("error in connecting to mongo", error);
+  }
 };
 module.exports = connectToMongo;
